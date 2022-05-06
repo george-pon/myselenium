a62bf14e80e62c20ea4db0b2082630586f683ac8
Jun Obama <george@yk.rim.or.jp>
2022-05-06 11:18:55 +0900

    mod

--- run-release-image.sh
+++ run-release-image.sh
@@ -7,6 +7,6 @@ function docker-run-myselenium() {
     ${WINPTY_CMD} docker run -i -t --rm \
         -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
         --cap-add SYS_ADMIN \
-        docker.io/georgesan/myselenium:latest
+        docker.io/georgesan/myselenium:latest "$@"
 }
 docker-run-myselenium
